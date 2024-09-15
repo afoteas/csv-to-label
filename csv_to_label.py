25d6ae9a42ed764a0a9055f0ddc7a96f1ea31def
afoteas <andreasfoteas@hotmail.com>
2024-09-16 00:38:53 +0300

    added dummy qrcode

--- csv_to_label.py
+++ csv_to_label.py
@@ -4,6 +4,7 @@ from barcode.writer import ImageWriter
 import imgkit
 import os
 import csv
+import qrcode
 
 
 # Get the directory of the running script
@@ -12,13 +13,9 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 wkhtmltoimage_path = '/usr/bin/wkhtmltoimage'  # Update with the correct path if necessary
 
 
-# Define fields to update in the HTML label
-product = 'Product A'
-date = '2025-10-15'
-position = 'A1'
-barcode_class = 'ean13'
-barcode_value = '1234567890128'
+
 barcode_path = os.path.join(script_dir, 'temp_barcode')  # Absolute path to barcode image
+qrcode_path = os.path.join(script_dir, 'temp_qrcode.png')  # Absolute path to barcode image
 html_path = os.path.join(script_dir, 'temp_label.html')  # Absolute path to output HTML file
 
 # Paths for input and output
@@ -43,9 +40,21 @@ def generate_label(product, date, position, barcode_class, barcode_value):
     ean = barcode.get_barcode_class(barcode_class)
     barcode_instance = ean(barcode_value, writer=ImageWriter())
     barcode_instance.save(barcode_path)
+
+        # Generate QR code image
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(barcode_value)
+    qr.make(fit=True)
+    qr_img = qr.make_image(fill='black', back_color='white')
+    qr_img.save(qrcode_path)
  
     # Render the template with field values and barcode image
-    rendered_html = template.render(product=product, date=date, position=position, barcode="barcode_path")
+    rendered_html = template.render(product=product, date=date, position=position, barcode=barcode_path+".png", qrcode=qrcode_path)
 
     # Write the updated HTML content to a temporary file
     with open(html_path, 'w') as temp_file:
@@ -58,8 +67,7 @@ def generate_label(product, date, position, barcode_class, barcode_value):
         'enable-local-file-access': '',
         'load-error-handling': 'ignore',
         'width': '400',
-        'height': '500',
-        'disable-smart-width': '',
+        'height': '800',
         'quality': '100',
         # 'crop-h': '100'  # Adjust height as needed to make sure content isn't clipped
     }
@@ -89,5 +97,8 @@ try:
     if os.path.exists(f"{barcode_path}.png"):
         os.remove(f"{barcode_path}.png")
         print(f"Temporary file {barcode_path}.png removed successfully.")
+    if os.path.exists(f"{qrcode_path}"):
+        os.remove(f"{qrcode_path}")
+        print(f"Temporary file {qrcode_path}  removed successfully.")
 except Exception as e:
     print(f"Error occurred while removing the temporary file: {e}")
